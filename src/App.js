9e9b1fe47e5aa0603c7f1f5ef5572e6215e1ef6e
Bruno Henrique <brunohvcarvalho@gmail.com>
2022-07-01 22:21:32 -0300

    finish crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import crud from './config/crud';
 
 function App() {
+  const [filterData, setFilter] = useState('');
+  const [id, setId] = useState('');
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    searchUser();
+  }, [filterData]);
+
+  const searchUser = () => {
+    const filters = [];
+
+    const filterName = {
+      property: 'first',
+      operator: '>=',
+      value: filterData
+    };
+
+    filters.push(filterName);
+
+    crud.get("users", filters).then(listUser => {
+      console.log(listUser);
+      setListUsers(listUser);
+    }).catch(console.error);
+  }
+
+  const addRegister = () => {
+    crud.save("users", mountUser())
+      .then(user => {
+        console.log('USER:', user);
+        searchUser();
+      }).catch(console.error);
+  }
+
+  const changeRegister = () => {
+    crud.save("users", mountUser(), id)
+      .then(user => {
+        console.log('USER:', user);
+        searchUser();
+      }).catch(console.error);
+  }
+
+  const deleteRegister = (id) => {
+    crud.delete("users", id)
+      .then(data => {
+        console.log('USER:', data);
+        searchUser();
+      }).catch(console.error);
+  }
+
+  const mountUser = () => {
+    return { first, last };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ID: <input value={id} onChange={e => { setId(e.target.value) }} /> <br />
+      FIRST: <input value={first} onChange={e => { setFirst(e.target.value) }} /><br />
+      LAST: <input value={last} onChange={e => { setLast(e.target.value) }} /><br />
+      <button onClick={addRegister} > Adicionar </button><br />
+      <button onClick={changeRegister} > Alterar </button><br /><br /><br />
+
+      FILTER: <input value={filterData} onChange={e => { setFilter(e.target.value) }} /><br /><br /><br />
+      {
+        listUsers.map(user => (<div>{user.id} {user.first}<button onClick={() => setId(user.id)}>E</button> <button onClick={() => deleteRegister(user.id)}>X</button></div>))
+      }
     </div>
   );
 }
